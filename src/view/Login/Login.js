8fb3735a2a9c5d87f869154e529735ac61f94a9c
prajktamhase <mhaseprajkta@gmail.com>
2023-10-02 15:02:11 +0800

    login page created

--- src/view/Login/Login.js
+++ src/view/Login/Login.js
@@ -1,10 +1,71 @@
 import './Login.css'
-const Login = () =>{
-    return(
-        <>
-        <h1 className="login">login</h1>
-        </>
+import { useState } from 'react';
+import showToast from 'crunchy-toast';
+import LoginImage from "./login.png"
+import Navbar from "./../../component/Navbar/Navbar";
+import Footer from "./../../component/Footer/Footer";
+const Login = () => {
+    const [fullName, setFullName] = useState('');
+    const [gender, setGender] = useState('Male');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    )
+    const SaveLocalStorage=()=>{
+        localStorage.setItem('byke',JSON.stringify())
+
+    }
+
+   
+
+
+
+return (
+    <>
+        <Navbar />
+        <div className='login-main'>
+
+
+            <div className='login'>
+                <div>
+                    <img src={LoginImage} className='login-img' />
+                </div>
+                <div>
+                    <form>
+
+                        <div className="info p-5 mt-5">
+
+                            <input type="mail"
+                                className='text'
+                                placeholder="Email"
+                                onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }} /><br />
+
+                            <input type="password"
+                                className='text'
+                                placeholder="Password"
+                                onChange={(e) => {
+                                    setPassword(e.target.value)
+                                }} /><br />
+                            <button tpye="button"
+                                onClick={showToast} className=' button'>Submit</button>
+                            {
+                                showToast('Login Successfully...!', 'success', 2000)
+                            }
+                        </div>
+                    </form>
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+
+        <Footer />
+    </>
+
+)
 }
 export default Login
