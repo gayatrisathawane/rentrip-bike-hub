1871d5af30009036daacbe8e26f1f637153f8193
yogitashete2412 <yogitashete24@gmail.com>
2023-10-03 08:21:08 +0530

    link to tag added to navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,40 +1,62 @@
-import './Navbar.css'
-import logo  from './logo-rentrip.png'
-const Navbar =() =>{
-    return(
-        <div>
-            <nav class="navbar navbar-expand-lg bg-dark sticky-top p-3 fs-4 nav1 ">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src={logo} className='logoimg'/></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-lg-auto">
-        <li class="nav-item">
-          <a class="nav-link text-light " aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link ms-5 text-light" href="/blog">Blog</a>
-        </li>
-        <li class="nav-item ms-5">
-          <a class="nav-link text-light" href="/faq">FAQ</a>
-        </li>
-        <li class="nav-item ms-5">
-          <a class="nav-link text-light " href="/review">Review</a>
-        </li>
-        <li class="nav-item ms-5">
-          <a class="nav-link text-light" to="/contact">Contact</a>
-        </li>
-        <li class="nav-item ms-5">
-          <a class="nav-link text-light" to="/login">Login</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-        </div>
+import "./Navbar.css";
+import logo from "./logo-rentrip.png";
+import { Link } from "react-router-dom";
 
-    )
-}
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark sticky-top p-3 fs-4 nav1 ">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="#">
+            <img src={logo} className="logoimg" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-lg-auto">
+              <li className="nav-item">
+                <Link className="nav-link text-light " aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link ms-5 text-light" to="/blog">
+                  Blog
+                </Link>
+              </li>
+              <li className="nav-item ms-5">
+                <Link className="nav-link text-light" to="/faq">
+                  FAQ
+                </Link>
+              </li>
+              <li className="nav-item ms-5">
+                <Link className="nav-link text-light " to="/review">
+                  Review
+                </Link>
+              </li>
+              <li className="nav-item ms-5">
+                <Link className="nav-link text-light" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item ms-5">
+                <Link className="nav-link text-light" to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 export default Navbar;
