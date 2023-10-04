9f962e1fc8f8c64079b2cd5e92df81f0d66c9f09
yogitashete2412 <yogitashete24@gmail.com>
2023-10-04 10:35:41 +0530

    initial changes in footer component

---
+++ src/component/ReviewTask/ReviewTask.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const ReviewTask=({id,title,description})=>{
+
+{
+  return (
+    <>
+       <h1>ReviewTask</h1> 
+       <p>{id}</p>
+       <p>{title}</p>
+       <p>{description}</p>
+       {/* <p>{name}</p>
+       <p>{profession}</p>
+       <p>{emoji}</p> */}
+
+
+    </>
+  )
+}
+}
+export default ReviewTask
--- src/view/Review/Review.js
+++ src/view/Review/Review.js
@@ -1,10 +1,47 @@
-import './Review.css'
-const Review = () =>{
-    return(
-        <div>
-            <h1 className='home'>Review</h1>
-        </div>
-    )
+import { useState } from "react";
+import Navbar from "./../../component/Navbar/Navbar";
+import CustomerCard from '../../component/CustomerCard/CustomerCard';
+import ReviewTask from "../../component/ReviewTask/ReviewTask";
+import "./Review.css";
 
-}
+const Review = ({}) => {
+    const [taskList, setTaskList] = useState("");
+    const [id, setId] = useState("");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+  return (
+    <>
+      <Navbar />
+      <CustomerCard/>
+
+      <div className="container">
+        <h1 className="text-center mt-5">Add Your Review Here</h1>
+
+<div className="d-flex justify-content-evenly mt-5">
+    <div>
+        <h1>Show Review</h1>
+        {/* {
+            taskList.map((taskItem,index)=>{
+
+                const {id, title, description} = taskItem;
+
+                return(<ReviewTask id={id} title={title} description={description}/>)
+                
+
+            })
+        }  */}
+
+    </div>
+    <div>
+        <h1>Add Review</h1>
+
+    </div>
+
+
+</div>
+      </div>
+    </>
+  );
+};
 export default Review;
