8666fdf2c2c21a9f749401b1a0b5cf8b1a2616b1
vivekpatil2001 <vivekshejole143@gmail.com>
2023-10-04 12:01:52 +0530

    added bikecards

--- src/view/BookBike/BookBike.js
+++ src/view/BookBike/BookBike.js
@@ -1,9 +1,24 @@
 import './BookBike.css'
-const BookBike = () =>{
-    return(
-        <>
-        BookBike
-        </>
+import bikeData from "./../../bikes/bike-data.json"
+import Navbar from '../../component/Navbar/Navbar';
+
+
+import Bikedatacard from '../../component/Bikedatacard/Bikedatacard';
+const BookBike = () => {
+    return (
+        <div>
+            <Navbar/>
+            
+            <div className='bookbike'>
+                {
+                    bikeData.map((bookbike, index) => {
+                        return <Bikedatacard key={index} id={bookbike.id} offer={bookbike.offer} left={bookbike.left} ImageUrl={bookbike.ImageUrl} title={bookbike.title} rating={bookbike.rating} price={bookbike.price} pickup={bookbike.pickup} dealertiming={bookbike.dealertiming} deposite={bookbike.dealertiming} deposite={bookbike.deposite} terms={bookbike.terms} viewdetails={bookbike.viewdetails} addtocard={bookbike.addtocard} />
+
+                    })
+                }
+            </div>
+        </div>
     )
 }
 export default BookBike;
+// id, offer, left, ImageUrl, title, rating, price, pickup, dealertiming, deposite, terms, viewdetails, addtocard 
